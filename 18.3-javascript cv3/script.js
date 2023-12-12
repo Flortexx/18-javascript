e1d6214e1a6e844bc7a50cdd205e0baeef075221
Flortexx <145441012+Flortexx@users.noreply.github.com>
2023-12-12 13:14:10 +0100

    Add files via upload

---
+++ 18.3-javascript cv3/script.js
@@ -0,0 +1,9 @@
+function forLoop(){
+
+    for (let i = 1; i <= 100; i++){
+        if (i % 2 == 0) {
+            document.getElementById("numberOutput").innerHTML +=" " + (i)
+        }
+    }
+
+}
